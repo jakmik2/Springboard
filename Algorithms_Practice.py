8525f9f9c366f4b97a7d7f8372d21cb3f5f450b4
jakmik2 <40475031+jakmik2@users.noreply.github.com>
2021-02-09 15:04:34 -0800

    second comit
    More Algorithm work (bfs), work on interview question (numeric to roman), and Springboard Class work (APIs, RSS, and web scraping)

--- Algorithms_Practice.py
+++ Algorithms_Practice.py
@@ -227,52 +227,122 @@ def quick_sort2(input_array, r):
     return input_array
 
 
+# Breadth first search code
+
+# The Following code is built from Joe James from this video URL: https://www.youtube.com/watch?v=-uR7BSfNJko
+# 2/5/2021
+
+class Vertex:
+    def __init__(self, n):
+        self.name = n
+        self.neighbors = list()
+
+        self.distance = 9999
+        self.color = 'black'
+
+    def add_neighbor(self, v):
+        if v not in self.neighbors:
+            self.neighbors.append(v)
+            self.neighbors.sort()
+
+
+class Graph:
+    vertices = {}
+
+    def add_vertex(self, vertex):
+        if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+            self.vertices[vertex.name] = vertex
+            return True
+        else:
+            return False
+
+    def add_edge(self, u, v):
+        if u in self.vertices and v in self.vertices:
+            for key, value in self.vertices.items():
+                if key == u:
+                    value.add_neighbor(v)
+                if key == v:
+                    value.add_neighbor(u)
+            return True
+        else:
+            return False
+
+    def print_graph(self):
+        for key in sorted(list(self.vertices.keys())):
+            print(key + str(self.vertices[key].neighbors) + ' ' + str(self.vertices[key].distance))
+
+    def bfs(self, vert):
+        q = list()
+        vert.distance = 0
+        vert.color = 'red'
+        for v in vert.neighbors:
+            self.vertices[v].distance = vert.distance + 1
+            q.append(v)
+
+        while len(q) > 0:
+            u = q.pop(0)
+            node_u = self.vertices[u]
+            node_u.color = 'red'
+
+            for v in node_u.neighbors:
+                node_v = self.vertices[v]
+                if node_v.color == 'black':
+                    q.append(v)
+                    if node_v.distance > node_u.distance + 1:
+                        node_v.distance = node_u.distance + 1
+
+
+# End of Paraphrased code 2/5/2021
+
+# Code to swap numbers with roman numerals
+
+def numerical_to_roman(int_input):
+
+    roman_numerals = {
+        1: "I",
+        5: "V",
+        10: "X",
+        50: "L",
+        100: "C",
+        500: "D",
+        1000: "M"
+    }
+
+    str_int = str(int_input)
+    digits = len(str_int)
+
+    roman_output = []
+
+    for i in range(0, digits):
+        check = int(str(1) + i * str(0))
+
+        storage = int(str_int[digits - 1 - i])
+
+        key = ''
+
+        if storage == 0:
+            pass
+        elif storage % 5 == 0 or storage == 1:
+            key = roman_numerals[storage * check]
+        elif (storage + 1) % 5 == 0 or (storage + 1) % 10 == 0:
+            key = roman_numerals[(storage + 1) * check]
+            key = roman_numerals[check] + key
+        elif (storage - 1) % 5 == 0:
+            key = roman_numerals[(storage - 1) * check]
+            key += roman_numerals[check]
+        else:
+            if storage < 5:
+                key = roman_numerals[check] * storage
+            else:
+                key = roman_numerals[5 * check]
+                key = key + roman_numerals[check] * (storage - 5)
+
+        roman_output.insert(0, key)
+
+    roman_output = ''.join(roman_output)
+
+    return roman_output
+
+
 if __name__ == '__main__':
-    array = [9, 7, 5, 11, 12, 2, 14, 3, 10, 6]
-    print(array)
-    print(quick_sort(array))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    pass
